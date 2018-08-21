2f82e3200f47d21f06f30178912c4f271e384759
yonatan astrachan <yonatank96@gmail.com>
2018-08-21 18:27:56 +0300

    restarted program, starting EvilCircle

--- main-finished (original).js
+++ main-finished (original).js
@@ -12,32 +12,18 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
-// define Shape constructor
 
-function Shape(x, y, velX, velY, exists){
+// define Ball constructor
+
+function Ball(x, y, velX, velY, color, size) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
-  this.exists = exists;
-}
-
-// EvilCircle definitions
-
-// define EvilCircle constructor
-
-
-
-// define Ball constructor
-
-function Ball(x, y, velX, velY,exists, color, size) {
-  Shape.call(this, x, y, velX, velY, exists)
-
   this.color = color;
   this.size = size;
 }
-Ball.prototype = Object.create(Shape.prototype);
-Ball.prototype.constructor = Ball;
+
 // define ball draw method
 
 Ball.prototype.draw = function() {
@@ -105,7 +91,6 @@ function loop() {
       random(0 + size,height - size),
       random(-7,7),
       random(-7,7),
-      true,
       'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
       size
     );
